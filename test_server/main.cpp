fc8b26d79727ba36e76b9d59c0a5e6602618c8de
Michael Uman <muman@magicleap.com>
2018-09-04 09:41:13 -0700

    New test server example
    * Started implementation of server example

---
+++ test_server/main.cpp
@@ -0,0 +1,24 @@
+//
+// Created by muman on 9/4/18.
+//
+#include <iostream>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <vector>
+#include <wait.h>
+#include <cassert>
+#include <cstring>
+#include <netinet/in.h>
+
+#include "server_utils.h"
+
+int main() {
+    printf("Hello world!\n");
+    return 0;
+}
